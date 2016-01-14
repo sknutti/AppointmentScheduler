46207a2eee62156596f228eae082f5838a36a5be
Scott Knutti <sknutti@gmail.com>
2016-01-14 13:18:51 -0700

    Initial Commit

---
+++ AppointmentScheduler/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  AppointmentScheduler
+//
+//  Created by Scott Knutti on 1/14/16.
+//  Copyright © 2016 Scott Knutti. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
